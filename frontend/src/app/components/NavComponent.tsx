0e1c56d89c37f2b5cfc7d14eed1b952c2317b0e0
gabrieleal-beep <gabriel.leal@impulsum.me>
2025-09-03 23:32:00 -0300

    test

--- frontend/src/app/components/NavComponent.tsx
+++ frontend/src/app/components/NavComponent.tsx
@@ -4,6 +4,8 @@ function NavComponent() {
     <nav>
         <div className="flex gap-4 text-black">
             <p><a href="/">Home</a></p>
+            <p><a href="/">Services</a></p>
+            <p><a href="/">Resources</a></p>
             <p><a href="/about">About</a></p>
             <p><a href="/contact">Contact</a></p>
         </div>
