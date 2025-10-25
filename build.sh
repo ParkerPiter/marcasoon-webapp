bdb6a25e309d7e3d895163070db19d69205fd630
jerome <skreamlight@gmail.com>
2025-10-24 19:34:05 -0600

    pg2 config 2

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@example.com DJANGO_SUPERUSER_PASSWORD='12345' python manage.py createsuperuser --noinput
